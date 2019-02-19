ae854889d4872a46e537b2905bde18dd0cfa1517
Ryan Lee <drdgvhbh@gmail.com>
2019-02-18 19:45:18 -0500

    Add typings to the error module
    Adding types helping modern IDEs and Text Editors like VSCode, catch errors in the code base before runtime. This is also useful for code completion tools

--- eosfactory/core/errors.py
+++ eosfactory/core/errors.py
@@ -4,9 +4,15 @@ import inspect
 
 import eosfactory.core.logger as logger
 import eosfactory.core.interface as interface
+from typing_extensions import Protocol
+from typing import cast, Match
 
 
-def validate(omittable):
+class Omittable(Protocol):
+    err_msg: str
+
+
+def validate(omittable: Omittable):
     '''Throw exception if validation fails.
     '''
     err_msg = omittable.err_msg
@@ -16,8 +22,12 @@ def validate(omittable):
     if "unknown key" in err_msg:
         raise AccountDoesNotExistError(omittable)
     elif "Error 3080001: Account using more than allotted RAM" in err_msg:
-        needs = int(re.search('needs\s(.*)\sbytes\shas', err_msg).group(1))
-        has = int(re.search('bytes\shas\s(.*)\sbytes', err_msg).group(1))
+        def extract_bytes(regex: str):
+            return int(cast(Match[str], re.search(
+                regex, err_msg)).group(1))
+        needs = extract_bytes('needs\s(.*)\sbytes\shas')
+        has = extract_bytes('bytes\shas\s(.*)\sbytes')
+
         raise LowRamError(needs, needs - has)
     elif "transaction executed locally, but may not be" in err_msg:
         pass
@@ -34,22 +44,23 @@ def validate(omittable):
         raise MissingRequiredAuthorityError(err_msg)
     elif "Duplicate transaction" in err_msg:
         raise DuplicateTransactionError(err_msg)
-    
+
     #######################################################################
     # NOT ERRORS
     #######################################################################
-    
+
     elif "Error 3120008: Key already exists" in err_msg:
-        pass                
+        pass
     else:
         raise Error(err_msg)
-        
+
 
 class Error(Exception):
     '''Base class for exceptions in EOSFactory.
     '''
-    def __init__(self, message, translate=True):
-        self.message = logger.error(message, translate)
+
+    def __init__(self, message: str, translate=True):
+        self.message: str = logger.error(message, translate)
         Exception.__init__(self, self.message)
 
 
@@ -59,71 +70,72 @@ class AccountDoesNotExistError(Error):
     Attributes:
         account: account argument: an ``Account`` object or account name.
     '''
-    def __init__(self, account):
+
+    def __init__(self, account: Omittable):
         self.account = account
         Error.__init__(
-            self, 
+            self,
             "Account ``{}`` does not exist in the blockchain."
-            .format(interface.account_arg(account)), 
+            .format(interface.account_arg(account)),
             True)
 
 
 class WalletDoesNotExistError(Error):
-    def __init__(self, wallet):
+    def __init__(self, wallet: Omittable):
         self.wallet = wallet
         Error.__init__(
-            self, 
+            self,
             "Wallet ``{}`` does not exist."
-            .format(interface.wallet_arg(wallet)), 
+            .format(interface.wallet_arg(wallet)),
             True)
 
 
 class WalletAlreadyExistsError(Error):
-    def __init__(self, wallet):
+    def __init__(self, wallet: Omittable):
         self.wallet = wallet
         Error.__init__(
-            self, 
+            self,
             "Wallet ``{}`` already exists."
-            .format(interface.wallet_arg(wallet)), 
+            .format(interface.wallet_arg(wallet)),
             True)
 
 
 class InvalidPasswordError(Error):
-    def __init__(self, wallet):
+    def __init__(self, wallet: Omittable):
         self.wallet = wallet
         Error.__init__(
-            self, 
+            self,
             "Invalid password for wallet {}"
-            .format(interface.wallet_arg(wallet)), 
+            .format(interface.wallet_arg(wallet)),
             True)
 
 
 class ContractRunningError(Error):
     def __init__(self):
         Error.__init__(
-            self, 
-            "Contract is already running this version of code", 
+            self,
+            "Contract is already running this version of code",
             True)
 
 
 class LowRamError(Error):
-    def __init__(self, needs_byte, deficiency_byte):
-        self.needs_kbyte =  needs_byte// 1024 + 1
+    def __init__(self, needs_byte: int, deficiency_byte: int):
+        self.needs_kbyte = needs_byte // 1024 + 1
         self.deficiency_kbyte = deficiency_byte // 1024 + 1
         Error.__init__(
-            self, 
+            self,
             "RAM needed is {}kB, deficiency is {}kB."
-            .format(self.needs_kbyte, self.deficiency_kbyte), 
-            True)   
+            .format(self.needs_kbyte, self.deficiency_kbyte),
+            True)
 
 
 class MissingRequiredAuthorityError(Error):
-    def __init__(self, message):
+    def __init__(self, message: str):
         Error.__init__(
             self, message, True)
 
 
 class DuplicateTransactionError(Error):
-    def __init__(self, message):
+    def __init__(self, message: str):
         Error.__init__(
             self, message, True)
